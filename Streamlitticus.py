7799ccd62f0e46bf5b9246d0d313f17d37266f9e
AlexStringfield <109487337+AlexStringfield@users.noreply.github.com>
2022-09-26 21:50:42 +0100

    Update Streamlitticus.py
    added simili

--- Streamlitticus.py
+++ Streamlitticus.py
@@ -4,4 +4,6 @@ streamlit.title('Matthew Smells!')
 
 
 streamlit.header('Really really bad')
-streamlit.text('It'+chr(39)+'s a real problem')
+streamlit.text('It'+chr(39)+'s a real problem, kind of smells like:')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
